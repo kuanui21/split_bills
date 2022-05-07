3dcf912b6a97baca7fe8f2a9339b0205d6c612df
Guan <kuanui21@gmail.com>
2022-05-07 19:35:46 +0800

    feat: add index page ui

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
